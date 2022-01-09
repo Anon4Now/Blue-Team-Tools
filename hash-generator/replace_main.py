e59bcf10108467d3054f1c278ad2bd1471d21cac
Anon4Now <tutkoda@gmail.com>
2022-01-08 23:29:38 -0500

    updated main file

--- hash-generator/replace_main.py
+++ hash-generator/replace_main.py
@@ -1,7 +1,6 @@
 import os, sys, time
 
 from watchingdog import Watchdog
-from user_cli import GetUserOptions
 from hash_generator import HashGenerator
 
 
@@ -20,52 +19,65 @@ if __name__ == '__main__':
 
     while True:
 
-        userInput = input("Would you like to start watching a folder? y/n")
+        fileDetected = False
 
-        userOpts = GetUserOptions()
-        outArgs = userOpts.checkOptions()
-
-        watcher = Watchdog(outArgs['folder'])
-
-        hashGen = HashGenerator("list.txt")
+        userInput = input("\r[+] Would you like to start watching a folder [y/n]? >> ")
 
         if userInput == 'y':
 
+            path = input("[+] Folder to be monitored, default is Downloads [enter file path or enter 'd' for default]? >> ")
+
+            if path == 'd':
+                path = 'C:/Users/tutko/Downloads'
+            else:
+                pass
+
+            watcher = Watchdog(path)
+
             # show time left to download file on CLI
-            print(f'Seconds Left to Download File...\n')
-            for i in range(30, -1, -1):
+            print(f'[+] Seconds Left to Download File...')
+            for i in range(5, -1, -1):
                 if not Watchdog.eventCheck:  # UPDATE THIS CLASS VAR FROM IMPORT
                     if i == 0:
-                        sys.stdout.write(f'No File Given')
                         sys.stdout.flush()
-                        time.sleep(1)
+                        sys.stdout.write(f'\r[-] No file detected, returning to start...')
+
                     else:
-                        sys.stdout.write(str(i) + f' \r')
+                        sys.stdout.write("\r" + str(i))
                         sys.stdout.flush()
                         time.sleep(1)
 
                 else:
-                    print(f'File seen - generating hashes...\r\n')
+                    print(f'[+] File seen - generating hashes...\n')
                     sys.stdout.flush()
+                    fileDetected = True
                     break
-            # call functions and sleep
-            time.sleep(10)
-            hashGen.startHash()
-            # time.sleep(3)
-            # vtHashCheck()
-            time.sleep(1)
-            clean_script()
 
-        elif userInput == 'f':
-            userInput = input("Do you want to exit? y/n")
+            if fileDetected:
+
+                # call functions and sleep
+                time.sleep(10)
+                hashGen.startHash()
+                # time.sleep(3)
+                # vtHashCheck()
+                time.sleep(1)
+                clean_script()
+            else:
+                sys.stdout.flush()
+                time.sleep(5)
+                continue
+
+        elif userInput == 'n':
+            userInput = input("[+] Do you want to exit [y/n]? >> ")
             if userInput == 'y':
+                print("[+] Closing program...")
                 exit()
             elif userInput == 'n':
                 continue
             else:
-                print("Unknown input, please enter 'y' or 'n'...")
+                print("[-] Unknown input, please enter 'y' or 'n'...")
                 continue
 
         else:
-            print("Unknown input, please enter 'y' or 'n'...")
+            print("[-] Unknown input, please enter 'y' or 'n'...")
             continue
